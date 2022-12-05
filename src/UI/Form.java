0fcebb25aaabb1de54eedcbc38487d60931eb8dd
jramso <josue.rsou2@gmail.com>
2022-12-05 16:50:01 +0000

    formator

--- src/UI/Form.java
+++ src/UI/Form.java
@@ -1,6 +1,14 @@
 package UI;
 
 public interface Form{
+    /**
+     * Metodo que implementa a exibição dos formulários
+     * podendo ter ou não uma interface GUI
+     * @since 1.0
+     * @autor Ernani L. R. Filho
+     * 
+     * 
+     */
     public abstract void exibe();
     
 }
